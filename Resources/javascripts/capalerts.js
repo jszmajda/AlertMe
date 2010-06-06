8e80e19b3f614afd53c4d0f88e9ccc3cbcec3657
Josh Szmajda <joshsz@gmail.com>
2010-06-05 21:26:53 -0400

    well, filtering isn't really working, and I'm too tired to understand why

--- Resources/javascripts/capalerts.js
+++ Resources/javascripts/capalerts.js
@@ -5,25 +5,28 @@ var currentLat = null;
 var currentLon = null;
 
 function wowIHateJSONP(r){
-  Ti.API.info(r);
   data = [];
   for(var i=0; i < r.alerts.length; i++){
     var al = r.alerts[i];
-    var row = Ti.UI.createTableViewRow({width:310, height: 'auto'});
-    var viewBase = Ti.UI.createView({backgroundColor: '#000', width: 310, height: 'auto', top:0, left:0});
-    for(var x = 0; x < al.info[0].category.length; x++){
-      var icon = Ti.UI.createImageView({url: '../images/' + al.info[0].category[0].toLowerCase() + '.png', top: (x * 40), left: 0, width: 40, height: 60 });
-      viewBase.add(icon);
+    if(categoryFilter == "All" || al.info[0].category[0] == categoryFilter){
+      var row = Ti.UI.createTableViewRow({width:310, height: 'auto'});
+      var viewBase = Ti.UI.createView({backgroundColor: '#000', width: 310, height: 'auto', top:0, left:0});
+      for(var x = 0; x < al.info[0].category.length; x++){
+        var icon = Ti.UI.createImageView({url: '../images/' + al.info[0].category[x].toLowerCase() + '.png', top: (x * 40), left: 0, width: 40, height: 60 });
+        viewBase.add(icon);
+      }
+      var lbl = Ti.UI.createLabel({text: al.info[0].headline, width: 310 - 45, height: 'auto', left: 45});
+      viewBase.add(lbl);
+      row.add(viewBase);
+
+      data.push(row);
+    } else {
+      data.push({title: 'not pushing '+al.info[0].category[0]+" due to categoryFilter "+ categoryFilter});
     }
-    var lbl = Ti.UI.createLabel({text: al.info[0].headline, width: 310 - 45, height: 'auto', left: 45});
-    viewBase.add(lbl);
-    row.add(viewBase);
-    //data.push({title: al.info[0].headline});
-    data.push(row);
   }
   tableView.setData(data);
 }
-function getAlerts(tableView, categoryFilter){
+function getAlerts(tableView){
   // TODO: Get latitude and longitude from phone
   var lat = 0;
   var lon = 0;
@@ -43,9 +46,6 @@ function getAlerts(tableView, categoryFilter){
     }
   };
   var url = api_url + '?output=json&callback=wowIHateJSONP';
-  if(categoryFilter != "All"){
-    url += '&info.category='+categoryFilter;
-  }
   xhr.open('GET', url);
   //xhr.setRequestHeader('Content-Type', 'application/json');
   //xhr.setRequestHeader('Accept', 'application/json');
